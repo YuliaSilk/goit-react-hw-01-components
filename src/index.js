366104a32427f78d74c83e8547a807600cbedb79
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-10-01 15:13:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-import Profile from 'components/Profile/profile';
+// import { Profile} from './components/Profile/profile';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
