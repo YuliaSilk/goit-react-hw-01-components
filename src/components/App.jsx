fd801853e246ccc46609e5b5be267d4aec47a2a3
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-10-01 15:12:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import user from "./data/user.json";
+import data from "./data/data.json";
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
+import { Profile } from 'Profile';
+import { Statistics } from 'Statistics';
+import { FriendList } from 'FriendList';
+import { TransactionHistory } from 'TransactionHistory';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Profile user={user} />
+    <Statistics data={data}/>
+    <FriendList friehds={friends}/>
+    <TransactionHistory transactions={transactions}/>
+    </>
+
   );
 };
+
+
+    // <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+    // >
+//       React homework template
+    // </div>
