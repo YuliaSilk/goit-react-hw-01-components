3e95df464d1c5faf33fb8a51c5ebd22dff9db8cf
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-10-01 15:51:58 +0300

    fixed import files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,10 @@ import data from "./data/data.json";
 import friends from "./data/friends.json";
 import transactions from "./data/transactions.json";
 
-import { Profile } from './Profile';
-import { Statistics } from './Statistics';
-import { FriendList } from './Profile';
-import { TransactionHistory } from './TransactionHistory';
+import { Profile } from './Profile/profile';
+import { Statistics } from './Statistics/statistics';
+import { FriendList } from './Profile/profile';
+import { TransactionHistory } from './TransactionHistory/transactionHistory';
 
 
 export const App = () => {
